690049535c7574e77f3fc53f6778cda9187aefec
Carles Gallardo <45995898+carles00@users.noreply.github.com>
2023-05-23 21:28:38 +0200

    turning, CarEntity structs

--- src/entity.h
+++ src/entity.h
@@ -4,6 +4,22 @@
 #include "mesh.h"
 #include "camera.h"
 
+struct sSpeedParameters {
+	float max_speed;
+	float max_acceleration;
+	float max_bracking;
+	float downforce;
+};
+
+struct sTurningParameters {
+	float max_angle;
+	float turning_acceleration;
+	float max_turn_speed;
+	float centering_acceleration; //speed that turn goes back to 0
+	float turning_speed_mult; //when the car goes slower, it turns more
+};
+
+
 class Entity {
 public:
 	Entity();
@@ -43,8 +59,9 @@ public:
 	
 	float speed;
 	float angle;
+	float rotation_speed;
 	
-	CarEntity(std::string name, Vector3 position, const char* meshf, const char* texturef, Shader* shader, float max_speed, float max_angle, float max_acceleration, float max_breacking, float downforce, float rotation_speed);
+	CarEntity(std::string name, Vector3 position, const char* meshf, const char* texturef, Shader* shader, sSpeedParameters speedParams, sTurningParameters tp);
 	//direction: +1 accel forward -1 brake 0 no acceleration
 	//turn: +1 Turn left -1 Trun Right 0 no turn
 	void move(int direction, int turn, float dt, Camera* camera);
@@ -53,13 +70,11 @@ public:
 	inline void goBackwards() { is_reversing = true; }
 	inline void goForwards() { is_reversing = false; }
 	inline std::string getGear() { return is_reversing ? "R": "1"; }
+	inline float getRotationSpeed() { return rotation_speed; }
 
 private:
-	float max_speed;
-	float max_angle;
-	float max_acceleration;
-	float max_breacking;
-	float downforce;
-	float rotation_speed;
+	sSpeedParameters speedParams;
+	sTurningParameters turnParams;
+
 	bool is_reversing;
 };
